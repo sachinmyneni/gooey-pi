d76806305fbd0be65c5c3cba130a0066c7b0ac89
Sachin Myneni <myneni@altair.com>
2020-11-04 13:30:40 -0800

    threaded version finally shows current data

--- get_gps_from_port.py
+++ get_gps_from_port.py
@@ -1,18 +1,32 @@
+import time
 import gps
+from datetime import datetime
+import random
 
 # Listen on port 2947 (gpsd) of localhost
 session = gps.gps("localhost", "2947")
 session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+location_file = '/home/pi/location'+datetime.now().strftime('%Y%m%d%H%M%S')+'.txt'
+random.seed()
 
 while True:
     try:
         report = session.next()
         # Wait for a 'TPV' report and display the current time
         # To see all report data, uncomment the line below
-        print(report)
+        #print(report)
         if report['class'] == 'TPV':
             if hasattr(report, 'time'):
                 print(report.time)
+            if hasattr(report, 'lon'):
+                print(report.lon)
+            if hasattr(report, 'lat'):
+                print(report.lat)
+            if hasattr(report, 'alt'):
+                print(report.alt)
+            with open(location_file,'a+') as lf:
+                lf.write(str(report.alt)+","+str(report.lat)+","+str(report.lon)+","+str(report.time)+"\n") 
+            time.sleep(random.randint(1,6))
     except KeyError:
         pass
     except KeyboardInterrupt:
---
+++ threaded_get_gps_from_port.py
@@ -0,0 +1,59 @@
+import threading
+import time
+import gps
+from datetime import datetime
+
+class GpsPoller(threading.Thread):
+
+   def __init__(self):
+       threading.Thread.__init__(self,daemon=True)
+       self.session = gps.gps("localhost", "2947")
+       self.session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+       self.current_value = None
+
+   def get_current_value(self):
+       return self.current_value
+
+   def run(self):
+       while True:
+           try:
+               report = self.session.next()
+               if report['class'] == 'TPV':
+                   if hasattr(report, 'time'):
+                       print(report.time)
+                   if hasattr(report, 'lon'):
+                       print(report.lon)
+                   if hasattr(report, 'lat'):
+                       print(report.lat)
+                   if hasattr(report, 'alt'):
+                       print(report.alt)
+                   self.current_value = report
+           except KeyError:
+               pass
+           except KeyboardInterrupt:
+               quit()
+           except StopIteration:
+               session = None
+               print("GPSD has terminated")
+       time.sleep(1) # tune this, you might not get values that quickly
+
+if __name__ == '__main__':
+
+   location_file = '/home/pi/location'+datetime.now().strftime('%Y%m%d%H%M%S')+'.txt'
+   gpsp = GpsPoller()
+   gpsp.start()
+   gps_attribs = ['time','alt','lon','lat']
+   # gpsp now polls every .2 seconds for new data, storing it in self.current_value
+   while True:
+       try:
+           with open(location_file,'a+') as lf:
+               result = gpsp.get_current_value()
+               if all(getattr(result,attr) for attr in gps_attribs):
+                   lf.write(str(result.alt)+","+str(result.lat)+","+str(result.lon)+","+str(result.time)+"\n") 
+                   # In the main thread, every 5 seconds print the current value
+                   print(gpsp.get_current_value())
+           time.sleep(5)
+       except AttributeError:
+           time.sleep(2)
+       except KeyboardInterrupt:
+           quit()
